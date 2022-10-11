7867b80b3d351db259986544b70f6e0e1cc6f0c0
Kopcsak Patrik <kopcsakpatrk1@gmail.com>
2022-10-11 12:22:27 +0200

    kesz

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 
 let tomb = [];
+let szazTomb = [];
+
+class Szazlabu
+{
+    #labakszama;
+
+    constructor(labakszama)
+    {
+        this.#labakszama = labakszama
+    }
+
+    toString()
+    {
+        console.log(this.#labakszama + " lábú százlábú")
+    }
+}
+
+
+function labakbolSzazlabuk(szazTomb)
+{
+   let kimenet = new Szazlabu(szazTomb);
+   console.log(kimenet);
+}
 
 document.addEventListener('DOMContentLoaded',()=>{
     for(let i = 0; i < 20; i++){
@@ -11,10 +34,19 @@ document.addEventListener('DOMContentLoaded',()=>{
         console.log(index+". "+e);
     });
 
- let ujTomb = tomb.filter(e => e%5==0);
- ujTomb.forEach((e,index)=>{
-    console.log(index+". "+e);
-    });
+ 
+    document.getElementById('valogatas').addEventListener('click', () => {
+        ujTomb = [];
+        ujTomb = tomb.filter(e => e%5==0);
+        ujTomb.forEach((e,index)=>{
+            console.log(index+1+". "+e);
+            });      
+    })
 
-
+   
+    document.getElementById('hozzaad').addEventListener('click', () => {
+        let szam = parseInt(document.getElementById('szam').value);
+        tomb.push(szam);
+        console.log(tomb);
+    })
 });
